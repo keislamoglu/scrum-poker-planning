8a33ddf947da05b1cbe3361843c9f16b1afbbd9e
keislamoglu <kadiremin@gmail.com>
2019-04-27 17:11:45 +0300

    refactor: better naming in entities

---
+++ src/app/entities/index.ts
@@ -0,0 +1,4 @@
+export * from './session';
+export * from './story';
+export * from './vote';
+export * from './voter';
--- src/app/entities/story.ts
+++ src/app/entities/story.ts
@@ -3,5 +3,5 @@ export interface Story {
   title: string;
   description: string;
   sessionId: string;
-  vote: number;
+  point: number; // 1-3, 5, 8, 13, 21, 34, 55, 89, 134, ?
 }
